ce96e9e87f5c9876a108c8e5a58e46f860ba8a3d
Vinay S <vinayshankar0369@gmail.com>
2024-03-29 12:34:16 +0530

    added login/signup functionality

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,16 @@ from rest_framework import serializers
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
+        fields = ['user', 'title']
+
+
+class AllTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
         fields = '__all__'
+
+
+class OneTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['completed']
--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', loginpage, name='loginpage'),
+    path('login/', login, name='login'),
+    path('signup/', signup, name='signup'),
+    path('todo-list/', home, name='home'),
     path('api/add-task/', add_task, name='add_task'),
     path('api/delete-task/<int:id>/', delete_task, name='delete_task'),
     path('api/display-tasks/', display_tasks, name='display_tasks'),
